e115f82b97cb3450289aa9e749e86e8329de09b8
Dali Kewara <01126010010@kpndom.com>
2026-07-13 14:13:34 +0700

    update

--- app/Http/Controllers/PerformanceReviewController.php
+++ app/Http/Controllers/PerformanceReviewController.php
@@ -79,16 +79,58 @@ class PerformanceReviewController extends Controller
         }
     }
 
-    public function formEdit($id) {
+    public function formEdit(Request $request) {
         try {
+            $contributorID = decrypt($request->contributor_id);
+            $period = $this->appService->appraisalPeriod();
+
+            if (!empty($request->period)) {
+                $period = $request->period;
+            }
+
+            $appraisalContributor = AppraisalContributor::where('id', $contributorID)->where('period', $period)->first();
+
+            if (empty($appraisalContributor)) {
+                Session::flash('error', "Appraisal contributor not found");
+                return redirect()->back();
+            }
+
+            $appraisal = Appraisal::with(['employee'])->where('id', $appraisalContributor->appraisal_id)->where('period', $period)->first();
+
+            if (empty($appraisal)) {
+                Session::flash('error', "Appraisal not found");
+                return redirect()->back();
+            }
+
+            if (empty($appraisal->employee)) {
+                Session::flash('error', "Employee not found");
+                return redirect()->back();
+            }
+
+            $employee = $appraisal->employee;
+            $employeeID = $employee->employee_id;
+            $employeeName = $employee->fullname;
+            $employeeJobLevel = $employee->job_level;
+            $employeeGroupCompany = $employee->group_company;
+            $employeeUnit = $employee->unit;
+            $employeeDesignationName = $employee->designation_name;
+
+            $performanceReviewTypes = PerformanceReviewType::where("is_active", true)->pluck("name");
+
             return view('pages.performance-review.form-edit', [
                 "parentLink" => "Appraisal",
                 "link" => "Performance Review",
+                "employee_id" => $employeeID,
+                "employee_name" => $employeeName,
+                "employee_job_level" => $employeeJobLevel,
+                "employee_group_company" => $employeeGroupCompany,
+                "employee_unit" => $employeeUnit,
+                "employee_designation_name" => $employeeDesignationName,
+                "performance_review_types" => $performanceReviewTypes,
             ]);
         } catch (Exception $e) {
-            return view('pages.performance-review.form-edit', [
-                'errors' => []
-            ]);
+            Session::flash('error', "General error: " . $e->getMessage());
+            return redirect()->back();
         }
     }
 
